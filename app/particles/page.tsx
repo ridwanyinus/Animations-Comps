94493ae493397436c1348be43a26d8de35b5f35f
Ridwan <ridwanyinusa15@gmail.com>
2024-08-25 12:06:12 +0100

    chore: change particles position

--- app/particles/page.tsx
+++ app/particles/page.tsx
@@ -8,13 +8,13 @@ const NasaEffects = dynamic(() => import("@/components/NasaEffects"), { ssr: fal
 const page = () => {
   return (
     <div className="relative h-screen w-full grid  items-start place-items-center">
-      <NasaEffects />
+      <ParticlesEffect />
       <div className="absolute  flex  flex-col justify-center items-center  bg-white py-1 w-1/2 mx-auto ">
         <Link rel="stylesheet" href="https://github.com/ridwanyinus/Animations-Comps" className="flex py-3 gap-x-1 hover:underline">
           Link to github repo <FaGithub size={20} />
         </Link>
       </div>
-      <ParticlesEffect />
+      <NasaEffects />
     </div>
   );
 };
