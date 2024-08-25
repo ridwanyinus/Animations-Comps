fc217d7be8785ae9cf8217881fe726444ce0cdf0
Ridwan <ridwanyinusa15@gmail.com>
2024-08-25 11:32:34 +0100

    fix: fix bug

---
+++ app/cursor/Cursor.tsx
@@ -0,0 +1,52 @@
+"use client";
+import React, { useEffect, useState, useRef } from "react";
+
+const Cursor: React.FC = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [blurPosition, setBlurPosition] = useState({ x: 0, y: 0 });
+  const blurTimeoutRef = useRef<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+      setPosition({ x: event.clientX, y: event.clientY });
+
+      if (blurTimeoutRef.current) {
+        clearTimeout(blurTimeoutRef.current);
+      }
+
+      blurTimeoutRef.current = setTimeout(() => {
+        setBlurPosition({ x: event.clientX, y: event.clientY });
+      }, 5); // Adjust delay as needed
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+      if (blurTimeoutRef.current) {
+        clearTimeout(blurTimeoutRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <main>
+      <div
+        className="cursor"
+        style={{
+          left: `${position.x}px`,
+          top: `${position.y}px`,
+        }}
+      />
+      <div
+        className="cursorBlur"
+        style={{
+          left: `${blurPosition.x}px`,
+          top: `${blurPosition.y}px`,
+        }}
+      />
+    </main>
+  );
+};
+
+export default Cursor;
--- app/cursor/page.tsx
+++ app/cursor/page.tsx
@@ -1,52 +1,13 @@
-"use client";
-import React, { useEffect, useState, useRef } from "react";
+import React from "react";
+import dynamic from "next/dynamic";
+const Cursor = dynamic(() => import("./Cursor"), { ssr: false });
 
-const Cursor: React.FC = () => {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-  const [blurPosition, setBlurPosition] = useState({ x: 0, y: 0 });
-  const blurTimeoutRef = useRef<NodeJS.Timeout | null>(null);
-
-  useEffect(() => {
-    const handleMouseMove = (event: MouseEvent) => {
-      setPosition({ x: event.clientX, y: event.clientY });
-
-      if (blurTimeoutRef.current) {
-        clearTimeout(blurTimeoutRef.current);
-      }
-
-      blurTimeoutRef.current = setTimeout(() => {
-        setBlurPosition({ x: event.clientX, y: event.clientY });
-      }, 5); // Adjust delay as needed
-    };
-
-    document.addEventListener("mousemove", handleMouseMove);
-
-    return () => {
-      document.removeEventListener("mousemove", handleMouseMove);
-      if (blurTimeoutRef.current) {
-        clearTimeout(blurTimeoutRef.current);
-      }
-    };
-  }, []);
-
+const page = () => {
   return (
-    <main>
-      <div
-        className="cursor"
-        style={{
-          left: `${position.x}px`,
-          top: `${position.y}px`,
-        }}
-      />
-      <div
-        className="cursorBlur"
-        style={{
-          left: `${blurPosition.x}px`,
-          top: `${blurPosition.y}px`,
-        }}
-      />
-    </main>
+    <div>
+      <Cursor />
+    </div>
   );
 };
 
-export default Cursor;
+export default page;
